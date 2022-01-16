1fef914290d3386c4d2e60a44cb94d816576ef98
Jamie <JamCal117@gmail.com>
2022-01-16 21:59:13 +0000

    Completed JoinGame (besides an issue with same nicknames) also re-added the home IP address for socket connection

--- app/src/main/java/uk/ac/swansea/codenames/JoinGame.kt
+++ app/src/main/java/uk/ac/swansea/codenames/JoinGame.kt
@@ -4,45 +4,55 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.EditText
 import android.os.Bundle
 import android.content.Intent
 import android.os.Handler
+import android.os.Looper
+import android.speech.tts.TextToSpeech
+import android.view.View
 import org.json.JSONObject
 import org.json.JSONArray
 import org.json.JSONException
-import android.view.ViewGroup
 import android.widget.Button
-import java.io.File
-import java.io.IOException
-import java.util.ArrayList
+import androidx.gridlayout.widget.GridLayout
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
+import java.util.*
 
-class JoinGame : AppCompatActivity() {
-    // TODO: Redesign layout.
+class JoinGame : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    //TODO: Sort problems with joining with same nickname. May be a server or client problem. Sometimes the joinFailNick isn't triggered
 
     private var constraintLayout: ConstraintLayout? = null
+    private var messageBox: GridLayout? = null
     private var scrollLinear: LinearLayout? = null
-    private var backButton: Button? = null
-    private var refreshButton: Button? = null
-    private var joinPrivateButton: Button? = null
-    private var joinGameTitle: TextView? = null
-    private var publicRoomText: TextView? = null
-    private var privateRoomText: TextView? = null
-    private var roomNameEdit: EditText? = null
-    private var passwordEdit: EditText? = null
+    private var backButton: MaterialButton? = null
+    private var refreshButton: MaterialButton? = null
+    private var joinPrivateButton: MaterialButton? = null
+    private var okButton: MaterialButton? = null
+    private var joinGameTitle: MaterialTextView? = null
+    private var publicRoomText: MaterialTextView? = null
+    private var privateRoomText: MaterialTextView? = null
+    private var messageText: MaterialTextView? = null
+    private var nicknameEdit: TextInputEditText? = null
+    private var roomNameEdit: TextInputEditText? = null
+    private var passwordEdit: TextInputEditText? = null
     private val publicJoinButtons = ArrayList<Button>()
     private var applicationBackgroundColour = -10921639
     private var menuButtonsColour = -8164501
     private var menuTextColour = -1
     private var username: String? = null
     private var validJoin = false
+    private var messageBoxOpen = false
+    private var textToSpeech: TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.join_game)
 
         constraintLayout = findViewById(R.id.constraintLayout)
+        messageBox = findViewById(R.id.messageBox)
         scrollLinear = findViewById(R.id.scrollLinear)
         backButton = findViewById(R.id.backButton)
         refreshButton = findViewById(R.id.refreshButton)
@@ -50,17 +60,27 @@ class JoinGame : AppCompatActivity() {
         joinPrivateButton = findViewById(R.id.joinPrivateButton)
         publicRoomText = findViewById(R.id.publicRoomText)
         privateRoomText = findViewById(R.id.privateRoomText)
+        nicknameEdit = findViewById(R.id.nicknameEdit)
         roomNameEdit = findViewById(R.id.roomNameEdit)
         passwordEdit = findViewById(R.id.passwordEdit)
+        okButton = findViewById(R.id.okButton)
+        messageText = findViewById(R.id.messageText)
+
+        textToSpeech = TextToSpeech(this, this)
 
         val preferences = this.getSharedPreferences("preferences", Context.MODE_PRIVATE)
 
         username = preferences.getString("username", "")
 
+        nicknameEdit?.setText(username)
+
         SocketConnection.socket.emit("getAllRooms")
 
         joinPrivateButton?.setOnClickListener {
             validJoin = true
+
+            SocketConnection.socket.close()
+            SocketConnection.socket.open()
 
             SocketConnection.socket.emit("joinRoom", username, roomNameEdit?.text.toString(),
                     passwordEdit?.text.toString())
@@ -75,9 +95,7 @@ class JoinGame : AppCompatActivity() {
                 b.isEnabled = false
             }
 
-            val handler = Handler()
-
-            handler.postDelayed({
+            Handler(Looper.getMainLooper()).postDelayed({
                 if (validJoin) {
                     val i = Intent(applicationContext, OnlineGame::class.java)
                     i.putExtra("username", username)
@@ -101,8 +119,26 @@ class JoinGame : AppCompatActivity() {
         }
 
         backButton?.setOnClickListener {
-            val i = Intent(applicationContext, OnlineSetup::class.java)
-            startActivity(i)
+            if (!messageBoxOpen) {
+                val i = Intent(applicationContext, OnlineSetup::class.java)
+                startActivity(i)
+            }
+        }
+
+        okButton?.setOnClickListener {
+            messageBoxOpen = false
+
+            messageBox?.visibility = View.INVISIBLE
+
+            for (b in publicJoinButtons) {
+                b.isEnabled = true
+            }
+
+            val editor = preferences.edit()
+
+            editor.putString("username", nicknameEdit?.text.toString())
+
+            editor.apply()
         }
 
         SocketConnection.socket.on("joinFail") { args ->
@@ -119,104 +155,156 @@ class JoinGame : AppCompatActivity() {
             publicJoinButtons.clear()
 
             val data = args[0] as JSONObject
-            val allRoomNames = ArrayList<String>()
-            val publicRooms = ArrayList<String>()
-            val jArray = data.names() as JSONArray
 
-            for (i in 0 until jArray.length()) {
-                try {
-                    allRoomNames.add(jArray.getString(i))
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            }
+            if (data.length() > 0) {
+                val allRoomNames = ArrayList<String>()
+                val publicRooms = ArrayList<String>()
+                val jArray = data.names() as JSONArray
 
-            for (s in allRoomNames) {
-                try {
-                    val j = data[s] as JSONObject
-                    val temp = j.toString()
-                    val parts = temp.split(",").toTypedArray()
-
-                    parts[0] = parts[0].replace("{\"roomName\":", "")
-                    parts[1] = parts[1].replace("\"password\":", "")
-                    parts[3] = parts[3].replace("\"closed\":", "")
-                    parts[3] = parts[3].replace("}", "")
-
-                    val roomName = parts[0].replace("\"", "")
-                    val password = parts[1].replace("\"", "")
-                    val closed = parts[3].toBoolean()
-
-                    if (password == "" && !closed) {
-                        publicRooms.add(roomName)
+                for (i in 0 until jArray.length()) {
+                    try {
+                        allRoomNames.add(jArray.getString(i))
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
                     }
-                } catch (e: JSONException) {
-                    e.printStackTrace()
                 }
-            }
 
-            runOnUiThread {
-                for (name in publicRooms) {
-                    val roomLinear = LinearLayout(applicationContext)
-                    roomLinear.orientation = LinearLayout.HORIZONTAL
+                for (s in allRoomNames) {
+                    try {
+                        val j = data[s] as JSONObject
+                        val temp = j.toString()
+                        val parts = temp.split(",").toTypedArray()
 
-                    val preferences = this.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+                        parts[0] = parts[0].replace("{\"roomName\":", "")
+                        parts[1] = parts[1].replace("\"password\":", "")
+                        parts[3] = parts[3].replace("\"closed\":", "")
+                        parts[3] = parts[3].replace("}", "")
 
-                    menuButtonsColour = preferences.getInt("menuButton", -8164501)
-                    menuTextColour = preferences.getInt("menuText", -1)
+                        val roomName = parts[0].replace("\"", "")
+                        val password = parts[1].replace("\"", "")
+                        val closed = parts[3].toBoolean()
 
-                    val joinButton = Button(applicationContext)
+                        if (password == "" && !closed) {
+                            publicRooms.add(roomName)
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
 
-                    joinButton.text = name
-                    joinButton.setTextColor(menuTextColour)
+                runOnUiThread {
+                    for (name in publicRooms) {
+                        val roomLinear = LinearLayout(applicationContext)
+                        roomLinear.orientation = LinearLayout.HORIZONTAL
 
-                    val buttonParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                        menuButtonsColour = preferences.getInt("menuButton", -8164501)
+                        menuTextColour = preferences.getInt("menuText", -1)
 
-                    joinButton.layoutParams = buttonParams
+                        val joinButton = Button(applicationContext)
+                        joinButton.text = name
+                        joinButton.setTextColor(menuTextColour)
+                        joinButton.setBackgroundColor(menuButtonsColour)
 
-                    joinButton.setBackgroundColor(menuButtonsColour)
+                        val buttonParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
 
-                    joinButton.setOnClickListener {
-                        validJoin = true
-                        SocketConnection.socket.emit("joinRoom", username, name, "")
+                        joinButton.layoutParams = buttonParams
 
-                        backButton?.isEnabled = false
-                        refreshButton?.isEnabled = false
-                        roomNameEdit?.isEnabled = false
-                        passwordEdit?.isEnabled = false
-                        joinPrivateButton?.isEnabled = false
+                        joinButton.setOnClickListener {
+                            validJoin = true
 
-                        for (b in publicJoinButtons) {
-                            b.isEnabled = false
+                            SocketConnection.socket.close()
+                            SocketConnection.socket.open()
+
+                            SocketConnection.socket.emit("joinRoom", username, name, "")
+
+                            backButton?.isEnabled = false
+                            refreshButton?.isEnabled = false
+                            roomNameEdit?.isEnabled = false
+                            passwordEdit?.isEnabled = false
+                            joinPrivateButton?.isEnabled = false
+
+                            for (b in publicJoinButtons) {
+                                b.isEnabled = false
+                            }
+
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                if (validJoin) {
+                                    val i = Intent(applicationContext, OnlineGame::class.java)
+                                    i.putExtra("username", username)
+                                    i.putExtra("roomName", name)
+                                    i.putExtra("isHost", false)
+
+                                    startActivity(i)
+                                } else {
+                                    backButton?.isEnabled = true
+                                    refreshButton?.isEnabled = true
+                                    roomNameEdit?.isEnabled = true
+                                    passwordEdit?.isEnabled = true
+                                    joinPrivateButton?.isEnabled = true
+                                }
+                            }, 3000)
                         }
 
-                        val handler = Handler()
+                        joinButton.setOnLongClickListener {
+                            speakOut(joinButton.text.toString())
+                            true
+                        }
 
-                        handler.postDelayed({
-                            if (validJoin) {
-                                val i = Intent(applicationContext, OnlineGame::class.java)
-                                i.putExtra("username", username)
-                                i.putExtra("roomName", name)
-                                i.putExtra("isHost", false)
-
-                                startActivity(i)
-                            } else {
-                                backButton?.isEnabled = true
-                                refreshButton?.isEnabled = true
-                                roomNameEdit?.isEnabled = true
-                                passwordEdit?.isEnabled = true
-                                joinPrivateButton?.isEnabled = true
-                            }
-                        }, 3000)
+                        publicJoinButtons.add(joinButton)
+                        scrollLinear?.addView(roomLinear)
+                        roomLinear.addView(joinButton)
                     }
-
-                    publicJoinButtons.add(joinButton)
-                    scrollLinear?.addView(roomLinear)
-                    roomLinear.addView(joinButton)
                 }
             }
         }
 
+        backButton?.setOnLongClickListener {
+            speakOut(backButton?.text.toString())
+            true
+        }
+
+        joinGameTitle?.setOnLongClickListener {
+            speakOut(joinGameTitle?.text.toString())
+            true
+        }
+
+        refreshButton?.setOnLongClickListener {
+            speakOut(refreshButton?.text.toString())
+            true
+        }
+
+        publicRoomText?.setOnLongClickListener {
+            speakOut(publicRoomText?.text.toString())
+            true
+        }
+
+        privateRoomText?.setOnLongClickListener {
+            speakOut(privateRoomText?.text.toString())
+            true
+        }
+
+        joinPrivateButton?.setOnLongClickListener {
+            speakOut(joinPrivateButton?.text.toString())
+            true
+        }
+
+        messageText?.setOnLongClickListener {
+            speakOut(messageText?.text.toString())
+            true
+        }
+
+        okButton?.setOnLongClickListener {
+            speakOut(okButton?.text.toString())
+            true
+        }
+
         updateColours()
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            textToSpeech!!.language = Locale.UK
+        }
     }
 
     override fun onBackPressed() {
@@ -224,11 +312,39 @@ class JoinGame : AppCompatActivity() {
     }
 
     private fun toggleMessageBox(message: String) {
+        messageBoxOpen = true
 
+        runOnUiThread {
+            messageBox?.visibility = View.VISIBLE
+            messageText?.text = message
+            nicknameEdit?.visibility = View.GONE
+
+            for (b in publicJoinButtons) {
+                b.isEnabled = false
+            }
+        }
     }
 
     private fun toggleNicknameBox(message: String) {
+        messageBoxOpen = true
 
+        runOnUiThread {
+            messageBox?.visibility = View.VISIBLE
+            messageText?.text = message
+            nicknameEdit?.visibility = View.VISIBLE
+
+            for (b in publicJoinButtons) {
+                b.isEnabled = false
+            }
+        }
+    }
+
+    private fun speakOut(message: String) {
+        val preferences = this.getSharedPreferences("preferences", Context.MODE_PRIVATE)
+
+        if (preferences.getBoolean("textToSpeech", true)) {
+            textToSpeech!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+        }
     }
 
     private fun updateColours() {
@@ -239,18 +355,23 @@ class JoinGame : AppCompatActivity() {
         menuTextColour = preferences.getInt("menuText", -1)
 
         constraintLayout?.setBackgroundColor(applicationBackgroundColour)
+        messageBox?.setBackgroundColor(applicationBackgroundColour)
 
         backButton?.setBackgroundColor(menuButtonsColour)
         refreshButton?.setBackgroundColor(menuButtonsColour)
         joinPrivateButton?.setBackgroundColor(menuButtonsColour)
+        okButton?.setBackgroundColor(menuButtonsColour)
 
         joinGameTitle?.setTextColor(menuTextColour)
         publicRoomText?.setTextColor(menuTextColour)
         privateRoomText?.setTextColor(menuTextColour)
+        messageText?.setTextColor(menuTextColour)
         roomNameEdit?.setTextColor(menuTextColour)
         passwordEdit?.setTextColor(menuTextColour)
+        nicknameEdit?.setTextColor(menuTextColour)
         backButton?.setTextColor(menuTextColour)
         refreshButton?.setTextColor(menuTextColour)
         joinPrivateButton?.setTextColor(menuTextColour)
+        okButton?.setTextColor(menuTextColour)
     }
 }
--- app/src/main/java/uk/ac/swansea/codenames/SocketConnection.kt
+++ app/src/main/java/uk/ac/swansea/codenames/SocketConnection.kt
@@ -6,5 +6,6 @@ import java.net.URI
 
 object SocketConnection {
     @JvmField
-    var socket: Socket = IO.socket(URI.create("https://codenamesserver.appspot.com"))
+    //var socket: Socket = IO.socket(URI.create("https://codenamesserver.appspot.com"))
+    var socket: Socket = IO.socket(URI.create("http://192.168.1.123:8080"))
 }
